5a90249166eb43e1130be9f1d75a4e784747c770
SaB-playmaker <54803829+SaB-playmaker@users.noreply.github.com>
2023-05-29 17:18:58 +0300

    Revert "Revert "Merge branch 'master' into Tr--Tomchakova---eventsPage""
    This reverts commit a2ae8e1900ad5454348bc8aa9f06cb4fc05c1f2d.

--- app/src/main/java/com/codingstuff/loginandsignup/LogInActivity.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/LogInActivity.kt
@@ -4,29 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.codingstuff.loginandsignup.databinding.ActivitySignInBinding
+import com.codingstuff.loginandsignup.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class SignInActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignInBinding
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
         firebaseAuth = FirebaseAuth.getInstance()
-        binding.textView.setOnClickListener {
+        binding.buttonSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
-        binding.button.setOnClickListener {
-            val email = binding.emailEt.text.toString()
-            val pass = binding.passET.text.toString()
+        binding.buttonLogIn.setOnClickListener {
+            val email = binding.editEmailAddress.text.toString()
+            val pass = binding.editPassword.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty()) {
 
@@ -41,14 +41,12 @@ class SignInActivity : AppCompatActivity() {
                 }
             } else {
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-
             }
         }
     }
 
     override fun onStart() {
         super.onStart()
-
         if(firebaseAuth.currentUser != null){
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
