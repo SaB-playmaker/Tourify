3c624810408f478e9161073c757dd818af90e572
vika-ui <114112448+vika-ui@users.noreply.github.com>
2023-05-30 00:35:31 +0300

    fix loyaltybank

--- app/src/main/java/com/codingstuff/loginandsignup/LogInActivity.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/LogInActivity.kt
@@ -4,17 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.codingstuff.loginandsignup.databinding.ActivitySignInBinding
+import com.codingstuff.loginandsignup.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class SignInActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignInBinding
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
