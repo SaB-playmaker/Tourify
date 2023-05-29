9806a6ce6d7735dc8455c155153365198a143fa0
vika-ui <114112448+vika-ui@users.noreply.github.com>
2023-05-30 00:59:36 +0300

    Revert "fix loyaltybank"
    This reverts commit 3c624810408f478e9161073c757dd818af90e572.

--- app/src/main/java/com/codingstuff/loginandsignup/SignInActivity.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/SignInActivity.kt
@@ -4,17 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.codingstuff.loginandsignup.databinding.ActivityLoginBinding
+import com.codingstuff.loginandsignup.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class LogInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
