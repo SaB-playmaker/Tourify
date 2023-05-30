5104e16a44352626073d6ac52bf5c800fb95c976
SaB-playmaker <54803829+SaB-playmaker@users.noreply.github.com>
2023-05-30 13:24:41 +0300

    LoyaltyBank added

--- app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loyalty_bank)
+        setContentView(R.layout.activity_main)
     }
     fun toursOnClickMethod(view: View) {
         val intent = Intent(this, ToursDataMock::class.java)
