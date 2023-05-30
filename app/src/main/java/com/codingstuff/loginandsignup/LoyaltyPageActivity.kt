dbe6e8685341eeb59ae465c475d4b654c1f06cd2
SaB-playmaker <54803829+SaB-playmaker@users.noreply.github.com>
2023-05-30 11:34:16 +0300

    Loyalty load added

---
+++ app/src/main/java/com/codingstuff/loginandsignup/LoyaltyPageActivity.kt
@@ -0,0 +1,11 @@
+package com.codingstuff.loginandsignup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoyaltyPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loyalty_page) // use your layout file here
+    }
+}
