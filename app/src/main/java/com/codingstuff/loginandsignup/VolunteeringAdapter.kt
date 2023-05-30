1f1f029760428c60b9936be052ed476ae3812aae
SaB-playmaker <54803829+SaB-playmaker@users.noreply.github.com>
2023-05-30 12:42:22 +0300

    Load Charity

--- app/src/main/java/com/codingstuff/loginandsignup/VolunteeringAdapter.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/VolunteeringAdapter.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import com.codingstuff.loginandsignup.databinding.EventsListBinding
+import com.codingstuff.loginandsignup.databinding.ActivityCaharityListItemBinding
 
 class VolunteeringAdapter(context: Context, dataArrayList: ArrayList<VolunteeringData>):
     ArrayAdapter<VolunteeringData>(context, R.layout.activity_caharity_list_item, dataArrayList){
@@ -15,12 +15,12 @@ class VolunteeringAdapter(context: Context, dataArrayList: ArrayList<Volunteerin
         val listData = getItem(position)!!
 
         if(view == null){
-            val binding =EventsListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            val binding = ActivityCaharityListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
             view = binding.root
             view.tag = binding
         }
 
-        val binding = view.tag as EventsListBinding
+        val binding = view.tag as ActivityCaharityListItemBinding
         with(binding) {
             listData.image?.let { picture.setImageBitmap(it) }
             textView.text = listData.name
